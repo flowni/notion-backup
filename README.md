6d34ea2ca245e194abec7c80361ef85a869109c4
Nic <nicholas.wolf@web.de>
2023-08-31 22:52:45 +0200

    upload to s3 bucket

--- README.md
+++ README.md
@@ -27,78 +27,9 @@ Automatically downloading backups from Notion requires two unique authentication
 
 ## Setup
 
-This assumes you are looking to set this up to back up Notion to GitHub.
+This assumes you are looking to set this up to back up Notion on a server and upload to AWS S3.
 
 1. Obtain the required values for the environment variables as explained above.
-2. Create a repo for your backup. You probably want it private.
-3. Set the `NOTION_TOKEN`, `NOTION_FILE_TOKEN` and `NOTION_SPACE_ID` environment variables as secrets in your GitHub repo.
-4. Give Actions write access to your repository: `Settings` > `Actions` > `General` > `Workflow permissions` > choose `Read and write permissions`
-5. Install the following under `.github/workflows/whatever.yml` in your repo.
-6. Configure the frequency by changing the `cron` value. You can use [Crontab.guru](https://crontab.guru/#0_*/4_*_*_*).
-7. Push and control your backup workflow from the `Actions` tab of your repository Github page.
-
-```yaml
-name: "Notion backup"
-
-on:
-  push:
-    branches:
-      - master
-  schedule:
-    -   cron: "0 */4 * * *"
-
-  # Allows you to run this workflow manually from the Actions tab
-  workflow_dispatch:
-
-jobs:
-  backup:
-    runs-on: ubuntu-latest
-    name: Backup
-    timeout-minutes: 15
-    steps:
-      - uses: actions/checkout@v3
-
-      - uses: actions/setup-node@v2
-        with:
-          node-version: '18'
-
-      - name: Delete previous backup
-        run: rm -rf markdown html *.zip
-
-      - name: Setup dependencies
-        run: npm install -g notion-backup
-
-      - name: Run backup
-        run: notion-backup
-        env:
-          NOTION_TOKEN: ${{ secrets.NOTION_TOKEN }}
-          NOTION_FILE_TOKEN: ${{ secrets.NOTION_FILE_TOKEN }}
-          NOTION_SPACE_ID: ${{ secrets.NOTION_SPACE_ID }}
-          NODE_OPTIONS: "--max-http-header-size 15000"
-
-      - name: Delete zips
-        run: |
-          rm -f *.zip
-          rm -f markdown/*-Part*.zip
-          rm -f html/*-Part*.zip
-
-      - name: Commit changes
-        run: |
-          git config user.name github-actions
-          git config user.email github-actions@github.com
-          git add .
-          git commit -m "Automated snapshot"
-          git push
-```
-
-## LFS Support
-
-You won't be able to backup files exceeding a size of 100MB unless you enable [Git LFS](https://git-lfs.github.com/). Add a file named `.gitattributes` at the root of your repository and add the following. If you want to support other file types, just add a new line for that file type.
-
-```
-*.zip filter=lfs diff=lfs merge=lfs -text
-*.png filter=lfs diff=lfs merge=lfs -text
-*.jpg filter=lfs diff=lfs merge=lfs -text
-*.jpeg filter=lfs diff=lfs merge=lfs -text
-*.psd filter=lfs diff=lfs merge=lfs -text
-```
+1. Create AWS S3 bucket and IAM user with write access to the bucket.
+1. Copy `.envrc.example` to `.envrc` and fill in the values: `cp .envrc.example .envrc`
+1. Create cronjob to run the backup script periodically. For example, to run the backup every night at 1.00AM, run `crontab -e` and add the following line: `0 1 * * * . /path/to/folder/.envrc && python3 /path/to/folder/notion-backup.js >> /path/to/folder/notion-backup.log 2>&1`
--- notion-backup.js
+++ notion-backup.js
@@ -1,25 +1,47 @@
 #!/usr/bin/env node
 /* eslint no-await-in-loop: 0 */
 
-let axios = require('axios')
-  , extract = require('extract-zip')
-  , { retry } = require('async')
-  , { createWriteStream } = require('fs')
-  , { mkdir, rm, readdir } = require('fs/promises')
-  , { join } = require('path')
-  , notionAPI = 'https://www.notion.so/api/v3'
-  , { NOTION_TOKEN, NOTION_FILE_TOKEN, NOTION_SPACE_ID } = process.env
-  , client = axios.create({
-      baseURL: notionAPI,
-      headers: {
-        Cookie: `token_v2=${NOTION_TOKEN}; file_token=${NOTION_FILE_TOKEN}`
-      },
-    })
-  , die = (str) => {
-      console.error(str);
-      process.exit(1);
-    }
-;
+let axios = require('axios'),
+  AWS = require('aws-sdk');
+(extract = require('extract-zip')),
+  ({ retry } = require('async')),
+  ({
+    createWriteStream,
+    mkdirSync,
+    rmdirSync,
+    readFileSync,
+    unlinkSync,
+  } = require('fs')),
+  ({ join } = require('path')),
+  (notionAPI = 'https://www.notion.so/api/v3'),
+  ({ NOTION_TOKEN, NOTION_SPACE_ID } = process.env),
+  (client = axios.create({
+    baseURL: notionAPI,
+    headers: {
+      Cookie: `token_v2=${NOTION_TOKEN}; file_token=${NOTION_FILE_TOKEN}`
+    },
+  })),
+  (die = (str) => {
+    console.error(str);
+    process.exit(1);
+  });
+
+const currentDate = new Date();
+
+function getTimestamp(currentDate) {
+  let year = currentDate.getFullYear();
+  let month = currentDate.getMonth() + 1;
+  let day = currentDate.getDate();
+  let hours = currentDate.getHours();
+  let minutes = currentDate.getMinutes();
+
+  month = month < 10 ? `0${month}` : month;
+  day = day < 10 ? `0${day}` : day;
+  hours = hours < 10 ? `0${hours}` : hours;
+  minutes = minutes < 10 ? `0${minutes}` : minutes;
+
+  return `${year}-${month}-${day}-${hours}-${minutes}`;
+};
 
 if (!NOTION_TOKEN || !NOTION_FILE_TOKEN || !NOTION_SPACE_ID) {
   die(`Need to have NOTION_TOKEN, NOTION_FILE_TOKEN and NOTION_SPACE_ID defined in the environment.
@@ -27,20 +49,22 @@ See https://github.com/darobin/notion-backup/blob/main/README.md for
 a manual on how to get that information.`);
 }
 
-async function post (endpoint, data) {
+async function post(endpoint, data) {
   return client.post(endpoint, data);
 }
 
-async function sleep (seconds) {
+async function sleep(seconds) {
   return new Promise((resolve) => {
     setTimeout(resolve, seconds * 1000);
   });
 }
 
 // formats: markdown, html
-async function exportFromNotion (format) {
+async function exportFromNotion(format, currentTimeStampString) {
   try {
-    let { data: { taskId } } = await post('enqueueTask', {
+    let {
+      data: { taskId },
+    } = await post('enqueueTask', {
       task: {
         eventName: 'exportSpace',
         request: {
@@ -54,17 +78,17 @@ async function exportFromNotion (format) {
       },
     });
     console.warn(`Enqueued task ${taskId}`);
-    let failCount = 0
-      , exportURL
-    ;
+    let failCount = 0,
+      exportURL;
     while (true) {
       if (failCount >= 5) break;
       await sleep(10);
-      let { data: { results: tasks } } = await retry(
-        { times: 3, interval: 2000 },
-        async () => post('getTasks', { taskIds: [taskId] })
+      let {
+        data: { results: tasks },
+      } = await retry({ times: 3, interval: 2000 }, async () =>
+        post('getTasks', { taskIds: [taskId] })
       );
-      let task = tasks.find(t => t.id === taskId);
+      let task = tasks.find((t) => t.id === taskId);
       // console.warn(JSON.stringify(task, null, 2)); // DBG
       if (!task) {
         failCount++;
@@ -73,10 +97,13 @@ async function exportFromNotion (format) {
       }
       if (!task.status) {
         failCount++;
-        console.warn(`No task status, waiting. Task was:\n${JSON.stringify(task, null, 2)}`);
+        console.warn(
+          `No task status, waiting. Task was:\n${JSON.stringify(task, null, 2)}`
+        );
         continue;
       }
-      if (task.state === 'in_progress') console.warn(`Pages exported: ${task.status.pagesExported}`);
+      if (task.state === 'in_progress')
+        console.warn(`Pages exported: ${task.status.pagesExported}`);
       if (task.state === 'failure') {
         failCount++;
         console.warn(`Task error: ${task.error}`);
@@ -90,43 +117,67 @@ async function exportFromNotion (format) {
     let res = await client({
       method: 'GET',
       url: exportURL,
-      responseType: 'stream'
+      responseType: 'stream',
     });
-    let stream = res.data.pipe(createWriteStream(join(process.cwd(), `${format}.zip`)));
+    let stream = res.data.pipe(
+      createWriteStream(
+        join(process.cwd(), `${currentTimeStampString}-${format}.zip`)
+      )
+    );
     await new Promise((resolve, reject) => {
       stream.on('close', resolve);
       stream.on('error', reject);
     });
-  }
-  catch (err) {
+  } catch (err) {
     die(err);
   }
 }
 
-async function run () {
-  let cwd = process.cwd()
-    , mdDir = join(cwd, 'markdown')
-    , mdFile = join(cwd, 'markdown.zip')
-    , htmlDir = join(cwd, 'html')
-    , htmlFile = join(cwd, 'html.zip')
-  ;
-  await exportFromNotion('markdown');
-  await rm(mdDir, { recursive: true, force: true });
-  await mkdir(mdDir, { recursive: true });
-  await extract(mdFile, { dir: mdDir });
-  await extractInnerZip(mdDir);
-  await exportFromNotion('html');
-  await rm(htmlDir, { recursive: true, force: true });
-  await mkdir(htmlDir, { recursive: true });
-  await extract(htmlFile, { dir: htmlDir });
-  await extractInnerZip(htmlDir);
+async function uploadFileToS3(fileName) {
+  const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  });
+
+  const fileContent = readFileSync(fileName);
+  const subFolderName = 'Notion/';
+
+  const params = {
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: `${subFolderName}${fileName}`,
+    Body: fileContent,
+  };
+
+  s3.upload(params, function (err, data) {
+    if (err) {
+      throw err;
+    }
+    console.log(`File uploaded successfully. ${data.Location}`);
+  });
 }
 
-async function extractInnerZip (dir) {
-  let files = (await readdir(dir)).filter(fn => /Part-\d+\.zip$/i.test(fn));
-  for (let file of files) {
-    await extract(join(dir, file), { dir });
-  }
+async function run() {
+  let cwd = process.cwd();
+  const currentTimeStampString = getTimestamp(currentDate);
+  console.log(`${currentTimeStampString} start script`);
+  const fileNameMarkdown = `${currentTimeStampString}-markdown.zip`;
+  const fileNameHtml = `${currentTimeStampString}-html.zip`;
+
+  // export of notion markdown and html
+  await exportFromNotion('markdown', currentTimeStampString);
+  await exportFromNotion('html', currentTimeStampString);
+
+  // upload to s3
+  console.log('start uploading to AWS S3');
+  uploadFileToS3(fileNameMarkdown);
+  uploadFileToS3(fileNameHtml);
+  console.log('uploaded');
+
+  // delete files from PC
+  unlinkSync(join(cwd, fileNameMarkdown));
+  unlinkSync(join(cwd, fileNameHtml));
+  console.log('removed files from disk');
+  console.log('finished!');
 }
 
 run();
